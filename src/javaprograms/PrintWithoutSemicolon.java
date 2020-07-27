1d0240d47b5dccab31b5d1d6975983caed19b6fe
s-vish <svishal077@gmail.com>
2020-07-27 12:35:17 +0530

    first program

---
+++ src/javaprograms/PrintWithoutSemicolon.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class PrintWithoutSemicolon {
+	
+	/**
+	 * How to print a statement WITHOUT using a semicolon?
+	 */
+	
+	public static void main(String[] args) {
+		
+		if(System.out.printf("Hello World")==null)
+		{
+			
+		}
+		
+	}
+
+}
