eb5ff033fc671c26422b4462fb5a14187712868d
s-vish <svishal077@gmail.com>
2020-07-27 12:50:35 +0530

    2nd commit

---
+++ src/javaprograms/PrintSemicolonWithoutUsingSemicolon.java
@@ -0,0 +1,14 @@
+package javaprograms;
+
+public class PrintSemicolonWithoutUsingSemicolon {
+
+	/**
+	 * How to Print a semicolon WITHOUT using a semicolon 
+	 */
+	public static void main(String[] args) {
+		
+		System.out.printf("%c",+59); 
+		// %c is character representation
+		//59 is ASCII value for semicolon
+	}
+}
