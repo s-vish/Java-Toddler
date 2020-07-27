beab47ce2392d0476a5fc0b1e738f5a3a7b35bed
s-vish <svishal077@gmail.com>
2020-07-27 12:57:43 +0530

    modified

--- src/javaprograms/PrintSemicolonWithoutUsingSemicolon.java
+++ src/javaprograms/PrintSemicolonWithoutUsingSemicolon.java
@@ -3,12 +3,24 @@ package javaprograms;
 public class PrintSemicolonWithoutUsingSemicolon {
 
 	/**
-	 * How to Print a semicolon WITHOUT using a semicolon 
+	 * How to Print a semicolon WITHOUT using a semicolon and semicolon at the end.
 	 */
-	public static void main(String[] args) {
-		
-		System.out.printf("%c",+59); 
+	
+	private void printSemicolonWithoutSemicolon()
+	{
+		System.out.printf("%c",+59);
 		// %c is character representation
 		//59 is ASCII value for semicolon
 	}
+	private void printSemicolonWithoutSemicolonAtTheEnd()
+	{
+		if(System.out.printf("%c",+59)==null) {}
+	}
+	public static void main(String[] args) {
+		PrintSemicolonWithoutUsingSemicolon obj = new PrintSemicolonWithoutUsingSemicolon();
+		obj.printSemicolonWithoutSemicolon();
+		obj.printSemicolonWithoutSemicolonAtTheEnd();
+		
+	}
+	
 }
