f11294c82186fa0ec9a3fa8ded86804076f8407a
s-vish <svishal077@gmail.com>
2020-07-30 14:00:46 +0530

    program - How to count vowels, consonants & special symbols in a String

---
+++ src/javaprograms/strings/CountVowelsConsonantsSpecialSymbols.java
@@ -0,0 +1,45 @@
+package javaprograms.strings;
+
+import java.util.Scanner;
+
+/**
+ * How to count vowels, consonants & special symbols in a String
+ */
+public class CountVowelsConsonantsSpecialSymbols {
+
+	public void count(String str) {
+		String userInput = str;
+		char y[] = userInput.toCharArray();
+		int size = y.length;
+		int count_vowel = 0;
+		int count_consonant = 0;
+		int count_special_symbols = 0;
+
+		int i = 0;
+		while (i != size) {
+			if ((y[i] >= 'a' && y[i] <= 'z') || y[i]>='A' && y[i]<='Z') {
+				if (y[i] == 'a' || y[i] == 'e' || y[i] == 'i' || y[i] == 'o' || y[i] == 'u'||
+						y[i] == 'A' || y[i] == 'E' || y[i] == 'I' || y[i] == 'O' || y[i] == 'U') {
+					count_vowel++;
+				} else {
+					count_consonant++;
+				}
+			} else {
+				count_special_symbols++;
+			}
+			++i;
+		}
+
+		System.out.println("number of Vowels are : " + count_vowel);
+		System.out.println("number of Consonants are : " + count_consonant);
+		System.out.println("number of Special Symbols are : " + count_special_symbols);
+	}
+
+	public static void main(String[] args) {
+		CountVowelsConsonantsSpecialSymbols obj = new CountVowelsConsonantsSpecialSymbols();
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter the text....");
+		obj.count(scr.nextLine().toString());
+	}
+
+}
